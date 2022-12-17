70e4c8e2b6431cf3271181e8980139fd82730355
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2022-12-17 22:37:27 +0100

    Update code.js

--- FrontEnd/code.js
+++ FrontEnd/code.js
@@ -1,11 +1,10 @@
 window.onload = function () {
   Particles.init({
-    selector: "background"
-
+    selector: ".background"
   });
 };
 const particles = Particles.init({
-  selector:  "bg-video-wrap",
+  selector: ".background",
   color: ["#03dac6", "#ff0266", "#000000"],
   connectParticles: true,
   responsive: [
